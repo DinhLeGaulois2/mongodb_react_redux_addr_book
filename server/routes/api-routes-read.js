1ecd0e54658de0dc991c24a22163698391e27618
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-14 06:14:48 -0400

    First Update

--- server/routes/api-routes-read.js
+++ server/routes/api-routes-read.js
@@ -1,9 +1,10 @@
-var Address = require('../data/address.js');
-var Branch = require('../data/branch.js');
-var BranchAddress = require('../data/branchAddress.js');
-var Customer = require('../data/customer.js');
-var CustomerAddress = require('../data/customerAddress.js');
-var Rental = require('../data/rental.js');
-var Vehicle = require('../data/vehicle.js');
+var Addresses = require('../data/addresses.js');
+var Contacts = require('../data/contacts.js');
+var Emails = require('../data/emails.js');
+var Groups = require('../data/groups.js');
+var Phones = require('../data/phones.js');
+var Portraits = require('../data/portraits.js');
+var Thumbnails = require('../data/thumbnails.js');
+var Twitter = require('../data/twitter.js');
 
 module.exports = function (app) {}
