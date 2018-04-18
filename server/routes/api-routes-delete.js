8587bec783081e563e0581a22b22c68d597ce331
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-17 23:34:05 -0400

    CRUD

--- server/routes/api-routes-delete.js
+++ server/routes/api-routes-delete.js
@@ -7,4 +7,52 @@ var Portraits = require('../models/portraits.js');
 var Thumbnails = require('../models/thumbnails.js');
 var Twitter = require('../models/twitter.js');
 
-module.exports = function (app) {}
+module.exports = function (app) {
+    app.delete("/api/delete/contact/:id", (req, res, next) => {
+        Contacts.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/group/:id", (req, res, next) => {
+        Groups.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/address/:id", (req, res, next) => {
+        Addresses.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/phone/:id", (req, res, next) => {
+        Phones.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/email/:id", (req, res, next) => {
+        Emails.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/portrait/:id", (req, res, next) => {
+        Portraits.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/thumbnail/:id", (req, res, next) => {
+        Thumbnails.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/twitter/:id", (req, res, next) => {
+        Twitters.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+}
--- server/routes/api-routes-update.js
+++ server/routes/api-routes-update.js
@@ -7,4 +7,79 @@ var Portraits = require('../models/portraits.js');
 var Thumbnails = require('../models/thumbnails.js');
 var Twitter = require('../models/twitter.js');
 
-module.exports = function (app) {}
+module.exports = function (app) {
+    app.put("/api/update/contact/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.name != undefined) obj.name = req.body.name
+        if (req.body.company != undefined) obj.company = req.body.company
+        if (req.body.title != undefined) obj.title = req.body.title
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/group/:id", (req, res, next) => {
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: { name: req.body.name } }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/address/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.type != undefined) obj.type = req.body.type
+        if (req.body.street != undefined) obj.street = req.body.street
+        if (req.body.city != undefined) obj.city = req.body.city
+        if (req.body.state != undefined) obj.state = req.body.state
+        if (req.body.zip_code != undefined) obj.zip_code = req.body.zip_code
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/phone/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.type != undefined) obj.type = req.body.type
+        if (req.body.number != undefined) obj.number = req.body.number
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/email/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.type != undefined) obj.type = req.body.type
+        if (req.body.address != undefined) obj.address = req.body.address
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/portrait/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.mime_type != undefined) obj.mime_type = req.body.mime_type
+        if (req.body.data != undefined) obj.data = req.body.data
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/thumbnail/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.mime_type != undefined) obj.mime_type = req.body.mime_type
+        if (req.body.data != undefined) obj.data = req.body.data
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+
+    app.put("/api/update/twitter/:id", (req, res, next) => {
+        let obj = {}
+        if (req.body.name != undefined) obj.name = req.body.name
+        if (req.body.location != undefined) obj.location = req.body.location
+        if (req.body.web != undefined) obj.web = req.body.web
+        if (req.body.bio != undefined) obj.bio = req.body.bio
+        Contacts.updateOne({ _id: req.params.id },
+            { $set: obj }).then(data => res.status(200).json("Update: Success!"))
+            .catch(next)
+    })
+}
