99582a3655f900c0de404ac189144d7e38762650
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-26 19:32:06 -0400

    React-Redux Setup

--- src/actions/testAction.js
+++ src/actions/testAction.js
@@ -4,39 +4,17 @@ import cst from '../constants/testConstant'
 
 const testAction = {
     testAPIRequest: () => {
-        let obj = [
-            {
-                name: "name1",
-                company: "company1",
-                title: "title1"
-            },
-            {
-                name: "name2",
-                company: "company2",
-                title: "title2"
-            },
-            {
-                name: "name3",
-                company: "company3",
-                title: "title3"
-            }
-        ]
-
-        let phone = {
-            type: "Home",
-            number: "408-258-1234",
-            contactId: "5ad67a9c8e329f19b461dc17"
-        }
         return (dispatch) => {
-            axios.get("/api/get/contacts")
+            axios.get("/api/test")
                 .then(data => {
-                    console.log("Result: " + JSON.stringify(data, null, 5))
-                }).catch(err => console.log(JSON.stringify(err, null, 5)))
-
-            // axios.get("/api/get/students")
-            //     .then(data => {
-            //         console.log("Result: " + JSON.stringify(data, null, 5))
-            //     })
+                    console.log("Test Request!")
+                    console.log("Result: " + JSON.stringify(data))
+                    dispatch({
+                        type: cst.TEST_RESULT,
+                        payload: data
+                    })
+                })
+                .catch(err => console.log("Request error: " + err))
         }
     }
 }
